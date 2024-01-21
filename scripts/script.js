dbc5bf9bd7e9a8a7aefb0483ece9f5345d2ea628
William-Lavoie <william.lavoie.3@ens.etsmtl.ca>
2024-01-21 17:57:30 -0500

    addNewCell()

--- scripts/script.js
+++ scripts/script.js
@@ -2,12 +2,34 @@ $(document).ready(function() {
 
     const GAME_IS_OVER = false;
 
+    const NB_CELLS = 16;
+
+    let grid = [];
+    let cellsArray = $("#grid-game").children();
 
 
+
+
+function addNewCell() {
     
+    // Generates two random integers representing a cell on the grid
+    let randomRow = parseInt(Math.random() * grid.length);
+    let randomColumn = parseInt(Math.random() * grid.length);
+
+    // The cell in the grid is mapped to a unique div element in cellsArray
+    let randomCell = $(cellsArray[randomRow*grid.length +randomColumn]);
+
+    // A newly created cell either takes 2 or 4 as a value
+    let newCellValue = (Math.round((Math.random()))+1) * 2;
+
+    randomCell.text(newCellValue);
+    
+    
+    
+    }
 
 
-
+addNewCell();
 })
 
 
